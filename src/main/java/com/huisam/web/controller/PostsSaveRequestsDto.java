58a180311eb9b16dc1759a90e3f5c21d3b2bfc24
huisam <huisam@naver.com>
2019-07-03 13:02:30 +0900

    add: h2 DataBase를 활용한 데이터 전달 - /posts로 Url Mapping - Getter와 Setter가 포함된 Dto를 만들어서 데이터베이스로 전송 - Entity Class와 Dto Class는 서로 분리하는게 올바르다 (Domain <-> View)

---
+++ src/main/java/com/huisam/web/controller/PostsSaveRequestsDto.java
@@ -0,0 +1,23 @@
+package com.huisam.web.controller;
+
+import com.huisam.web.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestsDto {
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
--- src/main/java/com/huisam/web/controller/WebRestController.java
+++ src/main/java/com/huisam/web/controller/WebRestController.java
@@ -1,13 +1,25 @@
 package com.huisam.web.controller;
 
+import com.huisam.web.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor // 필드의 생성자를 지정해주는 어노테이션
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestsDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
